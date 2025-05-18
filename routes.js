004edb56e6cf06386d53e4801e2d40d107127f65
Srikrishna-1046 <srikrishna.mca2024@adhiyamaan.in>
2025-05-18 18:47:53 +0530

    Add files via upload

---
+++ routes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const db = require('./db');
+
+router.get('/users', (req, res) => {
+  db.query('SELECT * FROM users', (err, results) => {
+    if (err) return res.status(500).send(err);
+    res.json(results);
+  });
+});
+
+router.post('/users', (req, res) => {
+  const { name, email } = req.body;
+  db.query('INSERT INTO users (name, email) VALUES (?, ?)', [name, email], (err, result) => {
+    if (err) return res.status(500).send(err);
+    res.json({ id: result.insertId, name, email });
+  });
+});
+
+module.exports = router;
